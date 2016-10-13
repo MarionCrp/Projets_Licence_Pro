b3ae1de554a59fb1fcf397d840d855fc8519543c
Marion <freja_sennyo@hotmail.com>
2016-10-13 15:12:21 +0200

    Add Tabata Timer

---
+++ java/faerun_battle/src/data/LetalStrikeException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+/**
+ *
+ * @author Marion
+ */
+public class LetalStrikeException extends Exception{
+
+    
+
+
+    
+}
