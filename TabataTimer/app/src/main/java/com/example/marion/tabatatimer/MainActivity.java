e635e73c42e32e401169598ed09f160601676d3e
Marion <freja_sennyo@hotmail.com>
2016-11-21 16:51:29 +0100

    ajout du design du timer

--- TabataTimer/app/src/main/java/com/example/marion/tabatatimer/MainActivity.java
+++ TabataTimer/app/src/main/java/com/example/marion/tabatatimer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.marion.tabatatimer;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import com.example.marion.tabatatimer.Program.Index;
 import com.example.marion.tabatatimer.Program.New;
+import com.example.marion.tabatatimer.data.Program;
 import com.example.marion.tabatatimer.data.ProgramDAO;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     public final static int MAIN_ACTIVITY_REQUEST = 1;
     public final static int NEW_ACTIVITY_REQUEST = 2;
     public final static int INDEX_ACTIVITY_REQUEST = 3;
+    public Program program = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +30,39 @@ public class MainActivity extends AppCompatActivity {
         if(programDao.count() > 0){
             test.setText(String.valueOf(programDao.selectFirstProgram().getTitle()));
         }
-        //Test d'affichage d'un timer.
-       /* new CountDownTimer(30000, 1000){
-            public void onTick(long millisUntilFinished) {
-                TextView chronometer = (TextView) findViewById(R.id.chronometer);
-                chronometer.setText(Long.toString(millisUntilFinished / 1000));
 
+        // Récupération de l'id du programme
+        String program_id;
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                program_id= null;
+            } else {
+                program_id= extras.getString("PROGRAM_ID");
             }
+        } else {
+            program_id= (String) savedInstanceState.getSerializable("PROGRAM_ID");
+        }
+        ProgramDAO programDAO = new ProgramDAO();
+        if(program_id != null){
+            program = programDAO.findById(Integer.parseInt(program_id));
 
-            public void onFinish() {
-               TextView chronometer = (TextView) findViewById(R.id.chronometer);
-                chronometer.setText("done!");
+            if(program != null){
+                System.out.println(program);
+                //Test d'affichage d'un timer.
+                new CountDownTimer(30000, 1000){
+                    public void onTick(long millisUntilFinished) {
+                        TextView chronometer = (TextView) findViewById(R.id.chronometer);
+                        chronometer.setText(Long.toString(millisUntilFinished / 1000));
+                    }
+
+                    public void onFinish() {
+                        TextView chronometer = (TextView) findViewById(R.id.chronometer);
+                        chronometer.setText("done!");
+                    }
+                }.start();
             }
-        }.start();*/
+        }
     }
 
     // Bouton pour créer un nouveau programme.
