3f70c8d1caedf742bbadcdb272abdfda8faae4d2
Marion <freja_sennyo@hotmail.com>
2016-11-24 10:22:15 +0100

    Chrono qui fonctionne. Mais il n'affiche pas la première seconde.

--- TabataTimer/app/src/main/java/com/example/marion/tabatatimer/MainActivity.java
+++ TabataTimer/app/src/main/java/com/example/marion/tabatatimer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.marion.tabatatimer;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,49 +19,49 @@ public class MainActivity extends AppCompatActivity {
     public final static int NEW_ACTIVITY_REQUEST = 2;
     public final static int INDEX_ACTIVITY_REQUEST = 3;
     public Program program = null;
+    public boolean work_session;
+    public boolean rest_session;
+    public int nb_cycle_done;
+    public final String WORK_COLOR = "#b30000";
+    public final String REST_COLOR = "#008000";
+    public final String ANNOUNCEMENT_COLOR = "#993366";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView test = (TextView)findViewById(R.id.textView2);
+        TextView test = (TextView) findViewById(R.id.textView2);
         ProgramDAO programDao = new ProgramDAO();
-        System.out.println(programDao.selectAll().size());
-        if(programDao.count() > 0){
+        if (programDao.count() > 0) {
             test.setText(String.valueOf(programDao.selectFirstProgram().getTitle()));
         }
-
         // Récupération de l'id du programme
         String program_id;
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
-            if(extras == null) {
-                program_id= null;
+            if (extras == null) {
+                program_id = null;
             } else {
-                program_id= extras.getString("PROGRAM_ID");
+                program_id = extras.getString("PROGRAM_ID");
             }
         } else {
-            program_id= (String) savedInstanceState.getSerializable("PROGRAM_ID");
+            program_id = (String) savedInstanceState.getSerializable("PROGRAM_ID");
         }
         ProgramDAO programDAO = new ProgramDAO();
-        if(program_id != null){
+
+        // Affichage du chrono si programme choisi :
+        if (program_id != null) {
+
             program = programDAO.findById(Integer.parseInt(program_id));
 
-            if(program != null){
-                System.out.println(program);
-                //Test d'affichage d'un timer.
-                new CountDownTimer(30000, 1000){
-                    public void onTick(long millisUntilFinished) {
-                        TextView chronometer = (TextView) findViewById(R.id.chronometer);
-                        chronometer.setText(Long.toString(millisUntilFinished / 1000));
-                    }
-
-                    public void onFinish() {
-                        TextView chronometer = (TextView) findViewById(R.id.chronometer);
-                        chronometer.setText("done!");
-                    }
-                }.start();
+            if (program != null) {
+                work_session = true;
+                rest_session = false;
+                nb_cycle_done = 1;
+                TextView annoucement = (TextView) findViewById(R.id.announcement);
+                annoucement.setText("WORK TIME");
+                setChrono();
             }
         }
     }
@@ -83,5 +84,55 @@ public class MainActivity extends AppCompatActivity {
 
     public void onCancel(View view){
         finish();
+    }
+
+    public void setChrono() {
+        if (!tabata_is_finished(program)){
+            TextView cycles_result = (TextView) findViewById(R.id.cycles_result);
+            cycles_result.setText(Integer.toString(nb_cycle_done) + "/" + Integer.toString(program.getNb_of_cycle()));
+            if (work_session == true) {
+                CountDownTimer countDownTimer = new CountDownTimer((program.getWork_time()) * 1000, 1000) {
+                    public void onTick(long millisUntilFinished) {
+                        TextView chrono = (TextView) findViewById(R.id.chrono);
+                        chrono.setText(Long.toString(millisUntilFinished / 1000));
+                        chrono.setTextColor(Color.parseColor(WORK_COLOR));
+                    }
+                    public void onFinish() {
+                        TextView announcement = (TextView) findViewById(R.id.announcement);
+                        announcement.setText("REST TIME");
+                        announcement.setTextColor(Color.parseColor(REST_COLOR));
+                        work_session = false;
+                        setChrono();
+                    }
+                }.start();
+            } else {
+                CountDownTimer countDownTimer = new CountDownTimer((program.getRest_time()) * 1000, 1000) {
+                    public void onTick(long millisUntilFinished) {
+                        TextView chrono = (TextView) findViewById(R.id.chrono);
+                        chrono.setText(Long.toString(millisUntilFinished / 1000));
+                        chrono.setTextColor(Color.parseColor(REST_COLOR));
+                    }
+                    public void onFinish() {
+                        TextView announcement = (TextView) findViewById(R.id.announcement);
+                        work_session = true;
+                        nb_cycle_done++;
+                        if(tabata_is_finished(program)){
+                            TextView chrono = (TextView) findViewById(R.id.chrono);
+                            announcement.setText("");
+                            chrono.setText("GOOD JOB !");
+                            chrono.setTextColor(Color.parseColor(ANNOUNCEMENT_COLOR));
+                        } else {
+                            announcement.setText("WORK TIME");
+                            announcement.setTextColor(Color.parseColor(WORK_COLOR));
+                        }
+                        setChrono();
+                    }
+                }.start();
+            }
+        }
+    }
+
+    public boolean tabata_is_finished(Program program) {
+        return program.getNb_of_cycle() < nb_cycle_done;
     }
 }
