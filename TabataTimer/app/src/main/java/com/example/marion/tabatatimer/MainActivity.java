f6c52cedb0d3ae9d6ee59a22fd0f8b27ec3a9d2d
Marion <freja_sennyo@hotmail.com>
2016-12-07 08:29:20 +0100

    pause/start button works

--- TabataTimer/app/src/main/java/com/example/marion/tabatatimer/MainActivity.java
+++ TabataTimer/app/src/main/java/com/example/marion/tabatatimer/MainActivity.java
@@ -22,9 +22,8 @@ public class MainActivity extends AppCompatActivity {
     public final static int INDEX_ACTIVITY_REQUEST = 3;
     public Program program = null;
     public String program_id;
-    public boolean work_session;
-    public int nb_cycle_done;
-    private Long max_time;
+
+    private static CountDownTimer timer;
     public final String WORK_COLOR = "#b30000";
     public final String REST_COLOR = "#008000";
     public final String ANNOUNCEMENT_COLOR = "#993366";
@@ -32,48 +31,50 @@ public class MainActivity extends AppCompatActivity {
     private static String STATE_NB_OF_CYCLE_DONE;
     private static String STATE_IS_WORK_SESSION;
     private static String STATE_PROGRAM_ID;
-    private static CountDownTimer countDownTimer;
-    private long remaining_time;
-    private boolean has_been_stop = false;
+
+    //
+    public String session_type;
+    public int nb_cycle_done;
+    public long remaining_time;
+    public String state = "ready";
     private Bundle savedInstanceState;
+
+    //
+    public String current_timer_name;
+
+    // Elément de design :
+    public TextView tv_announcement;
+    public TextView tv_cycles;
+    public TextView tv_chrono;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialisation des données à sauvegarder.
-        max_time = Long.valueOf(0);
-        nb_cycle_done = 1;
-        work_session = true;
-
-        // Récupération de l'id du programme
         Bundle extras = getIntent().getExtras();
+        tv_announcement = (TextView) findViewById(R.id.announcement);
+        tv_cycles = (TextView) findViewById(R.id.cycles_result);
+        tv_chrono = (TextView) findViewById(R.id.chrono);
+
         if (extras == null) {
             program_id = null;
         } else {
+            tv_announcement.setText("PRESS START TO BEGIN!");
+            current_timer_name = "work";
+            nb_cycle_done = 1;
             Button start_button = (Button) findViewById(R.id.start_button);
             Button pause_button = (Button) findViewById(R.id.pause_button);
             start_button.setVisibility(View.VISIBLE);
             pause_button.setVisibility(View.VISIBLE);
             program_id = extras.getString("PROGRAM_ID");
-            program = program = ProgramDAO.findById(Integer.parseInt(program_id));
+            program = ProgramDAO.findById(Integer.parseInt(program_id));
+            remaining_time = program.getWork_time();
         }
-
-
-        TextView annoucement = (TextView) findViewById(R.id.announcement);
-
-        if (program != null && has_been_stop == false) {
-            nb_cycle_done = 1;
-            annoucement.setText("PRESS START TO BEGIN!");
-        }
-
-        // Affichage du chrono si programme choisi :
     }
 
     // Bouton pour créer un nouveau programme.
     public void onNew(View view){
-
         Intent intent = new Intent(this, New.class);
         /*intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivityForResult(intent, NEW_ACTIVITY_REQUEST);*/
@@ -87,94 +88,100 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, INDEX_ACTIVITY_REQUEST);
     }
 
-    public void onStart(View view){
-        if (program_id != null) {
-            program = ProgramDAO.findById(Integer.parseInt(program_id));
-            setChrono(work_session, program.getWork_time());
-            remaining_time = program.getWork_time();
-        }
+    public void onStart(View view) {
+        launchTimer();
 
-        if (savedInstanceState != null) {
-            max_time = savedInstanceState.getLong(STATE_MILLIUNTILFINISHED);
-            nb_cycle_done = savedInstanceState.getInt(STATE_NB_OF_CYCLE_DONE);
-            work_session = savedInstanceState.getBoolean(STATE_IS_WORK_SESSION);
+    }
+
+    public void launchTimer(){
+        if(state == "ready"){
+            if (current_timer_name.equals("work")) remaining_time = program.getWork_time() * 1000;
+            else if (current_timer_name.equals("rest")) remaining_time = program.getRest_time() * 1000;
         }
-        setChrono(work_session, remaining_time);
+        state = "ready";
+        //else if (current_timer_name.equals("Paused!")) remaining_time = saveCurrentTimerName;
+        if( !tabata_is_finished(program)) {
+            timer = new CountDownTimer(remaining_time, 10) {
+
+                public void onTick(long millisUntilFinished) {
+                    remaining_time = millisUntilFinished;
+                    miseAJour();
+                }
+
+                public void onFinish() {
+                    remaining_time = 0;
+                    if (current_timer_name.equals("work")) {
+                        current_timer_name = "rest";
+                        launchTimer();
+                    } else if (current_timer_name.equals("rest")) {
+                        current_timer_name = "work";
+                        nb_cycle_done++;
+                        miseAJour();
+                        launchTimer();
+                    }
+                }
+            }.start();
+        }
+        miseAJour();
     }
 
     public void onPauseButton(View view){
-        System.out.println(countDownTimer);
-        countDownTimer.cancel();
-        //System.out.println(remaining_time);
+        state = "on pause";
+        if (timer != null) {
+            timer.cancel();
+        }
     }
 
     public void onCancel(View view){
         finish();
     }
 
-    // setChrono(time, done_cycle, work_type)
-    public void setChrono(boolean is_work_session, long saved_remaining_time) {
-        if (!tabata_is_finished(program)){
-            TextView cycles_result = (TextView) findViewById(R.id.cycles_result);
-            cycles_result.setText(Integer.toString(nb_cycle_done) + "/" + Integer.toString(program.getNb_of_cycle()));
-            if (work_session == true) {
-                //max_time = Long.valueOf(program.getWork_time()) * 1000;
-
-                countDownTimer = new CountDownTimer( saved_remaining_time * 1000 , 1) {
-                    public void onTick(long millisUntilFinished) {
-                        TextView chrono = (TextView) findViewById(R.id.chrono);
-                        chrono.setText(Long.toString(millisUntilFinished / 1000) + "," + Long.toString(millisUntilFinished % 1000));
-                        chrono.setTextColor(Color.parseColor(WORK_COLOR));
-                        remaining_time = (millisUntilFinished / 1000) + (millisUntilFinished % 1000);
-                    }
-                    public void onFinish() {
-                        TextView announcement = (TextView) findViewById(R.id.announcement);
-                        announcement.setText("REST TIME");
-                        announcement.setTextColor(Color.parseColor(REST_COLOR));
-                        work_session = false;
-                        setChrono(work_session, remaining_time);
-                    }
-                }.start();
+    // Mise à jour graphique
+    private void miseAJour() {
+        String chrono_color;
+        String annoucement_text;
+        if (tabata_is_finished(program)) {
+            tv_chrono.setText("Good Job!");
+            tv_announcement.setText("");
+            tv_cycles.setText("");
+        } else {
+            if (current_timer_name == "work") {
+                chrono_color = WORK_COLOR;
+                annoucement_text = "WORK TIME";
+            } else if (current_timer_name == "rest") {
+                chrono_color = REST_COLOR;
+                annoucement_text = "REST TIME";
             } else {
-                max_time = Long.valueOf(program.getRest_time()) * 1000;
-                countDownTimer = new CountDownTimer(max_time, 1) {
-                    public void onTick(long millisUntilFinished) {
-                        TextView chrono = (TextView) findViewById(R.id.chrono);
-                        chrono.setText(Long.toString(millisUntilFinished / 1000) + "," + Long.toString(millisUntilFinished % 1000));
-                        chrono.setTextColor(Color.parseColor(REST_COLOR));
-                        remaining_time = (millisUntilFinished / 1000) + (millisUntilFinished % 1000);
-                    }
-                    public void onFinish() {
-                        TextView announcement = (TextView) findViewById(R.id.announcement);
-                        work_session = true;
-                        nb_cycle_done++;
-                        if(tabata_is_finished(program)){
-                            TextView chrono = (TextView) findViewById(R.id.chrono);
-                            announcement.setText("");
-                            chrono.setText("GOOD JOB !");
-                            chrono.setTextColor(Color.parseColor(ANNOUNCEMENT_COLOR));
-                        } else {
-                            announcement.setText("WORK TIME");
-                            announcement.setTextColor(Color.parseColor(WORK_COLOR));
-                        }
-                        setChrono(work_session, remaining_time);
-                    }
-                }.start();
+                chrono_color = "red";
+                annoucement_text = "";
             }
+            // Décompositions en secondes et minutes
+            int secs = (int) (remaining_time / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) (remaining_time % 1000);
+
+            // Affichage du "timer"
+            tv_chrono.setText("" + mins + ":"
+                    + String.format("%02d", secs) + ":"
+                    + String.format("%03d", milliseconds));
+            tv_chrono.setTextColor(Color.parseColor(chrono_color));
+            tv_announcement.setText(annoucement_text);
+            tv_announcement.setTextColor(Color.parseColor(chrono_color));
+            tv_cycles.setText(nb_cycle_done + " / " + program.getNb_of_cycle());
         }
+
     }
 
-
-    //TODO ::: FAIRE FONCTION PAUSE
     public boolean tabata_is_finished(Program program) {
-        return program.getNb_of_cycle() < nb_cycle_done;
+        return nb_cycle_done > program.getNb_of_cycle();
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState){
-        savedInstanceState.putLong(STATE_MILLIUNTILFINISHED, max_time);
+        savedInstanceState.putLong(STATE_MILLIUNTILFINISHED, remaining_time);
         savedInstanceState.putInt(STATE_NB_OF_CYCLE_DONE, nb_cycle_done);
-        savedInstanceState.putBoolean(STATE_IS_WORK_SESSION, work_session);
+        savedInstanceState.putString(STATE_IS_WORK_SESSION, session_type);
         savedInstanceState.putString(STATE_PROGRAM_ID, program_id);
 
         super.onSaveInstanceState(savedInstanceState);
@@ -185,7 +192,7 @@ public class MainActivity extends AppCompatActivity {
         super.onRestoreInstanceState(savedInstanceState);
         remaining_time = savedInstanceState.getLong(STATE_MILLIUNTILFINISHED);
         nb_cycle_done = savedInstanceState.getInt(STATE_NB_OF_CYCLE_DONE);
-        work_session = savedInstanceState.getBoolean(STATE_IS_WORK_SESSION);
+        session_type = savedInstanceState.getString(STATE_IS_WORK_SESSION);
         program_id = savedInstanceState.getString(STATE_PROGRAM_ID);
     }
 }
